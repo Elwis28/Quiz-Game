92fa98c90d2690ad66b743bf4d2feb319ef0706a
laszlo.kiss <laszlo.kiss@antavo.com>
2024-12-04 14:54:50 +0100

    Add index.html to path in server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 let teamTokens = {};
