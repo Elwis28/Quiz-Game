b75dee4d28f780edfc3a2a9502ff1903fd4eb766
laszlo.kiss <laszlo.kiss@antavo.com>
2024-12-04 14:19:28 +0100

    add build folder, file path configured

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const { Server } = require('ws');
 const crypto = require('crypto');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build'));
+});
 
 let teamTokens = {};
 
