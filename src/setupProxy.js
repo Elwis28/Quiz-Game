3eb23342d884c6b5022a270b7d6795c40b7c92fa
laszlo.kiss <laszlo.kiss@antavo.com>
2024-12-05 18:38:57 +0100

    modify backend url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
         createProxyMiddleware('/api', {
-            target: process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://quiz-game-v2-9046345b6d4d.herokuapp.com',
+            target: process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://shrouded-river-15106-69fad24cfbc6.herokuapp.com',
             changeOrigin: true,
         })
     );
